42b643d888306176b609f563048af4c6f35a23d5
AinoraZ <AinoraZ@gmail.com>
2015-12-12 20:02:34 +0200

    Removed fucked up tile set

--- src/Main.hx
+++ src/Main.hx
@@ -7,24 +7,6 @@ class Main extends luxe.Game {
 
     override function config(config:luxe.AppConfig) {
 
-        config.preload.textures.push({id:'assets/Tiles/1.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/2.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/3.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/4.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/5.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/6.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/7.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/8.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/9.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/10.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/11.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/12.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/13.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/14.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/15.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/16.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/17.png', filter_min:nearest, filter_mag:nearest });
-        config.preload.textures.push({id:'assets/Tiles/18.png', filter_min:nearest, filter_mag:nearest });
         config.preload.textures.push({id:'assets/tileset.png', filter_min:nearest, filter_mag:nearest });
 
         config.preload.texts.push({id:'assets/tilemap.tmx'});
