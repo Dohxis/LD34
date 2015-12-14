7c3f812a2c543aef642fd4d3e47b2fb3b799f318
Edvinas <edvinas.senda@gmail.com>
2015-12-14 20:25:30 +0200

    Start is now 1st level/merge

--- src/Main.hx
+++ src/Main.hx
@@ -58,7 +58,7 @@ class Main extends luxe.Game {
         jump.volume = 0.5;
 
         state.add( new Menu() );
-        state.add( new Game(3, 0) );
+        state.add( new Game(1, 0) );
         state.set('menu');
         music();
 
